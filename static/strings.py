f21730a684399949b22d80fef5b1bb45aae27ea2
Drabek Jan <drabek.honza@gmail.com>
2018-03-10 16:02:35 +0100

    Extract strings from code

---
+++ static/strings.py
@@ -0,0 +1,2 @@
+INTENT_LAUNCH_MSG = "Hey, I'm Otto Investment bot, I' here to inform you about your investments. Do you want me to tell you a report on your portfolio? Or maybe information about specific stock? "
+INTENT_LAUNCH_REPROMPT = "Go on, tell me what can I do for you."
