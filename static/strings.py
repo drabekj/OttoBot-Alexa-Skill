db6f9e5c25201d098d897e4b11e41c2b183a33b9
Drabek Jan <drabek.honza@gmail.com>
2018-03-12 01:40:55 +0100

    Change strings, price now has $

--- static/strings.py
+++ static/strings.py
@@ -1,11 +1,11 @@
 REQUEST_LAUNCH_MSG = "Hey, I'm Otto Investment bot, I' here to inform you about your investments. Do you want me to tell you a report on your portfolio? Or maybe information about specific stock? "
 REQUEST_LAUNCH_REPROMPT = "Go on, tell me what can I do for you."
 
-INTENT_STOCK_PRICE_MSG = "The price of {0} is {1}."
+INTENT_STOCK_PRICE_MSG = "The price of {0} is ${1}."
 INTENT_STOCK_PRICE_MSG_FAIL = "Sorry, there was a problem getting data for {}"
 
 INTENT_WATCHLIST_REPORT_MSG_INTRO = "Here is your watchlist:"
-INTENT_WATCHLIST_REPORT_MSG_BODY = " stock {} is valued at {}"
+INTENT_WATCHLIST_REPORT_MSG_BODY = " stock {} is valued at ${}"
 INTENT_WATCHLIST_EMPTY_MSG = "Your watchlist is empty. "
 
 INTENT_GENERAL_REPROMPT = "Is there something else I can help you with?"
