7c9dcf5028d12164dd94043374d79e0a1a1d3ff1
Drabek Jan <drabek.honza@gmail.com>
2018-03-26 16:16:13 +0200

    Implement RSS Feed reader

---
+++ app/utils/FeedReader/__init__.py
@@ -0,0 +1,18 @@
+import feedparser
+
+
+class FeedReader(object):
+    def __init__(self, ticker):
+        self.data = feedparser.parse(
+            f'http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol={ticker}')
+
+    def get_articles_titles(self, limit=3):
+        """ Get list of article headings.
+        :param limit: limit the number of titles received
+        :return: [string]
+        """
+        for index, article in enumerate(self.data['entries']):
+            if index >= limit:
+                break
+
+            print(article['title'])
