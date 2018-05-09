6c17aff03d7fecbf2a3d92625379b4eaaf99d8f2
Drabek Jan <drabek.honza@gmail.com>
2018-05-09 18:55:13 +0200

    Add more educational terms

--- app/intent_education/__init__.py
+++ app/intent_education/__init__.py
@@ -8,9 +8,19 @@ def handle_education(request):
     term = request.get_slot_value('term')
 
     message = {
-        'Market Capitalization': strings_edu.TERM_MARKET_CAP,
+        'Liquid market': strings_edu.TERM_LIQUIDITY,
+        'Yield': strings_edu.TERM_YIELD,
+        'Spread': strings_edu.TERM_SPREAD,
+        'Dividend': strings_edu.TERM_DIVIDEND,
+        'Broker': strings_edu.TERM_BROKER,
+        'Bear Market': strings_edu.TERM_BEAR_MARKET,
+        'Bull Market': strings_edu.TERM_BULL_MARKET,
+        'BID': strings_edu.TERM_BID,
+        'ASK': strings_edu.TERM_ASK,
         'Price Earnings Ratio': strings_edu.TERM_PE_RATIO,
+        'Market Capitalization': strings_edu.TERM_MARKET_CAP,
         'Equity': strings_edu.TERM_EQUITY,
+        'IPO': strings_edu.TERM_IPO,
     }.get(term, strings.INTENT_EDU_IN_CONSTRUCTION.format(term))
     reprompt_message = strings.REQUEST_LAUNCH_REPROMPT
 
